da9e9ba4e398ddada8787439ad4eeb2a89a0f86f
Daria Borodina <86297040+DEBorodina@users.noreply.github.com>
2021-08-30 12:08:19 +0300

    Add files via upload

---
+++ scripts/model.js
@@ -0,0 +1,11 @@
+export const model = {
+    todos: [],
+    getLocalStorage: function(){
+        if(localStorage.getItem("todos") != null){
+        model.todos = JSON.parse(localStorage.getItem('todos'));
+        } 
+    },
+    setLocalStorage: function(){
+        localStorage.setItem('todos', JSON.stringify(model.todos));
+    },
+}
